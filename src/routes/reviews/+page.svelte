fab4f88a3f8b44ee7768d94c73f75c61127fe06d
townsag <townsag@gmail.com>
2024-02-26 13:03:03 -0800

    some code cleanup

--- src/routes/reviews/+page.svelte
+++ src/routes/reviews/+page.svelte
@@ -3,11 +3,13 @@
     import RatingsDots from "$lib/components/Ratings_dots.svelte";
 </script>
 
-<section class="bg-pale-green flex flex-col p-8 items-center">
-    <h1 class="text-5xl text-royal-green px-2 mb-2 max-w-5xl w-full">Reviews:</h1>
-    <ul class="p-2 space-y-4 max-w-5xl w-full">
+
+<section class="bg-pale-green flex flex-col p-8 max-w-5xl">
+    <h1 class="text-5xl text-royal-green px-2 mb-2  w-full">Reviews:</h1>
+    <ul class="p-2 space-y-4 w-full">
         {#each data.posts as post}
-            <li class="flex flex-col sm:flex-row justify-between sm:space-x-2 sm:items-center bg-pale-yellow rounded-md shadow-sm p-3">
+            <li class="flex flex-col bg-pale-yellow rounded-md shadow-sm p-3 max-w-max
+                        sm:flex-row sm:space-x-2 sm:items-center sm:max-w-none sm:justify-between">
                 <div class="flex flex-col space-y-1">
                     <a href={post.path} class="text-3xl text-royal-green">{post.meta.shop_name}</a>
                     <p class=" text-royal-green">Reviewed: {post.meta.date}</p>
@@ -15,7 +17,6 @@
                 <div class="mt-2 sm:mt-0">
                     <RatingsDots scores={post.meta.scores} is_inline={true}/>
                 </div>
-                
             </li>
         {/each}
     </ul>
